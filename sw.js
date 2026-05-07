a75f9e0b8178d0aa92b7d110986b230defeabf40
uner_engineering <mustafauner86@gmail.com>
2026-05-07 16:20:12 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'siber-kasa-v4'; 
+const CACHE_NAME = 'siber-kasa-v5'; 
 const ASSETS = [
   'index.html',
   'manifest.json',
@@ -6,13 +6,9 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((res) => res || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then((r) => r || fetch(event.request)));
 });
